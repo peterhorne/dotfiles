e2fe8600ac48180e2ea624a690a1cec572196c89
Peter Horne-Khan <peterhorne92@gmail.com>
2023-05-14 22:17:25 +0100

    ruby: remove gemrc

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd)
 ln -s $ABSPATH/.ackrc ~/.ackrc
 ln -s $ABSPATH/.agignore ~/.agignore
 ln -s $ABSPATH/.ctags ~/.ctags
-ln -s $ABSPATH/.gemrc ~/.gemrc
 ln -s $ABSPATH/.gitconfig ~/.gitconfig
 ln -s $ABSPATH/.hushlogin ~/.hushlogin
 ln -s $ABSPATH/.oh-my-zsh ~/.oh-my-zsh
